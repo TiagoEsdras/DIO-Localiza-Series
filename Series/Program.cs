268c2ff89d38c83587a4747382ce52aa99b9b155
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-02-24 11:58:32 -0300

    Implementando metodo ListarSeries

--- Series/Program.cs
+++ Series/Program.cs
@@ -4,9 +4,75 @@ namespace Series
 {
     class Program
     {
+        static SerieRepositorio repositorio = new SerieRepositorio();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        //InserirSerie();
+                        break;
+                    case "3":
+                       // AtualizarSerie();
+                        break;
+                    case "4":
+                       // ExcluiSerie();
+                        break;
+                    case "5":
+                        //VisualizarSerie();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                        
+                }
+
+            }
+        }
+
+        public static void ListarSeries()
+        {
+            Console.WriteLine("Lista de Series");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                Console.WriteLine("#ID {0}: - {1}", serie.retornaId(), serie.retornaTitulo());
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada");
+
+            Console.WriteLine("1 - Listar Series");
+            Console.WriteLine("2 - Inserir Serie");
+            Console.WriteLine("3 - Atualizar Serie");
+            Console.WriteLine("4 - Excluir Serie");
+            Console.WriteLine("5 - Visualizar Serie");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair");
+
+            string opcao = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcao;
         }
     }
 }
